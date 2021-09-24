6c6b02ab4ac4c9d530588c6b489d38f9698cf653
danizidio <38110599+danizidio@users.noreply.github.com>
2021-09-24 16:27:51 -0300

    Update EnemyIA.cs

--- Scripts/Enemies/EnemyIA.cs
+++ Scripts/Enemies/EnemyIA.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 //The url below shows a video of the enemy identifying the target and then identifying the action to act properly
 //https://onedrive.live.com/?authkey=%21AKLhqwqflagGla8&cid=8EB27566A27E4C95&id=8EB27566A27E4C95%21742274&parId=8EB27566A27E4C95%21742268&o=OneUp
 
+//ENUM for IA behaviours
 public enum IAStates
 {
     PATROLLING,
@@ -49,16 +50,16 @@ public class EnemyIA : MonoBehaviour
         pivotSpeed = -e.Atributes.MoveSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
         CompairDistance();
-        //CompairDistance(1, Players[1]);
+        
         IAPrimaryState(CurrentState);
 
         CurrentState = NextState;
     }
 
+//Looking for GameObjects with the specified tag
     public void FindingPlayers()
     {
         Players = GameObject.FindGameObjectsWithTag("Player");
@@ -103,6 +104,9 @@ public class EnemyIA : MonoBehaviour
         }
     }
 
+//Compair the distance between the objects stored in the Player Array
+//Set the characterToFocus gameobject
+//Change the State of this enemy if some player is closer or far enough
     public float CompairDistance()
     {
         foreach (var item in Players)
@@ -139,6 +143,7 @@ public class EnemyIA : MonoBehaviour
         return pivotDist;
     }
 
+//Enemy movement
     public void Movimento()
     {
         //animBehaviour.SetBool("walking", true);
@@ -162,6 +167,8 @@ public class EnemyIA : MonoBehaviour
         }
     }
 
+//If player is closer enough this enemy will change his state to engaging
+//Changing the current animation and reading the focused player attack states 
     public void Engaging()
     {
         e.GetComponent<MovingBase>().XAxyz = 0;
@@ -177,11 +184,15 @@ public class EnemyIA : MonoBehaviour
             e.GetComponent<Animator>().SetBool("Engage", true);
         }
     }
+   
+//Just to verify the current state in case of need   
     public IAStates GetCurrentIAState()
     {
         return CurrentState;
     }
 
+//Method to change the next state for this enemy
+//The next state will become the current state on the next frame of the update
     public void ChangeIAState(IAStates newState)
     {
         PreviousState = CurrentState;
